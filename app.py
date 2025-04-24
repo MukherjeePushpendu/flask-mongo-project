493a3d9cf07246b906783c12e4aaf609dfacc133
puspa <pushpendumukharjee@gmail.com>
2025-04-24 21:18:19 +0530

    Add Item ID field to To-Do form

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 import json
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# MongoDB setup
+client = MongoClient('mongodb://localhost:27017/')
+db = client['todoapp']
+todos = db['todos']
 
 @app.route('/api', methods=['GET'])
 def get_data():
@@ -36,6 +42,32 @@ def form():
 @app.route('/todo')
 def todo():
     return render_template('todo.html')
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo():
+    try:
+        item_id = request.form.get('itemId')
+        item_name = request.form.get('itemName')
+        item_description = request.form.get('itemDescription')
+        
+        if not item_id or not item_name or not item_description:
+            return render_template('todo.html', error="All fields are required")
+        
+        # Check if item ID already exists
+        if todos.find_one({'id': item_id}):
+            return render_template('todo.html', error="Item ID already exists")
+        
+        # Insert into MongoDB
+        todo_item = {
+            'id': item_id,
+            'name': item_name,
+            'description': item_description
+        }
+        todos.insert_one(todo_item)
+        
+        return render_template('todo.html', success="Todo item added successfully!")
+    except Exception as e:
+        return render_template('todo.html', error=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
